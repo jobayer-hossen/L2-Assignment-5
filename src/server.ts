c77f276a050432077d4af268448f9a39f3570f35
jobayer-hossen <emon45088@gmail.com>
2025-08-18 21:19:52 +0600

    more error

--- src/server.ts
+++ src/server.ts
@@ -1,71 +1,77 @@
 /* eslint-disable no-console */
-import { Server } from "http";
-import mongoose from "mongoose";
+import { Server } from "http"
+
+import mongoose from "mongoose"
 import app from "./app";
 import { envVars } from "./app/config/env";
-import { connectRedis } from "./app/config/redis.config";
 import { seedSuperAdmin } from "./app/utils/seedSuperAdmin";
 
-let server: Server;
 
-// Start the server and connect to the database (only for local/dev)
+
+let server: Server
+
+
 const startServer = async () => {
-  try {
-    await mongoose.connect(envVars.DB_URL);
-    console.log("✅ Connected To MongoDb Successfully");
-
-    await connectRedis();
-    await seedSuperAdmin();
-
-    server = app.listen(envVars.PORT, () => {
-      console.log(`🚀 Server is Running Locally On Port ${envVars.PORT}`);
-    });
-  } catch (error) {
-    console.log("❌ Server startup error:", error);
-  }
-};
-
-// Run only if not on Vercel (NODE_ENV !== "production")
-if (process.env.NODE_ENV !== "production") {
-  startServer();
+    try {
+        await mongoose.connect(envVars.DB_URL);
+        console.log("Connected To MongoDb")
+        server = app.listen(envVars.PORT, () => {
+            console.log(`Server is Running On Port ${envVars.PORT}`)
+        })
+    } catch (error) {
+        console.log(error)
+    }
 }
 
-// Export app for Vercel serverless function
-export default app;
+(async () => {
+    await startServer()
+    await seedSuperAdmin()
+})()
 
-// Handle graceful shutdown
-process.on("SIGTERM", () => {
-  console.log("Signal Termination Happened..! Server Is Shutting Down..!");
-  if (server) {
-    server.close(() => process.exit(1));
-  } else {
-    process.exit(1);
-  }
-});
+process.on("SIGTERM", (err) => {
+    console.log("Signal Termination Happened...! Server Is Shutting Down !", err)
+    if (server) {
+        server.close(() => {
+            process.exit(1)
+        })
+    }
+
+    process.exit(1)
+
+})
 
 process.on("SIGINT", () => {
-  console.log("Signal Interrupt Happened..! Server Is Shutting Down..!");
-  if (server) {
-    server.close(() => process.exit(1));
-  } else {
-    process.exit(1);
-  }
-});
+    console.log("I am manually Closing the server! Server Is Shutting Down !")
+    if (server) {
+        server.close(() => {
+            process.exit(1)
+        })
+    }
 
+    process.exit(1)
+
+})
 process.on("unhandledRejection", () => {
-  console.log("Unhandled Rejection Happened..! Server Is Shutting Down..!");
-  if (server) {
-    server.close(() => process.exit(1));
-  } else {
-    process.exit(1);
-  }
-});
+
+    console.log("Unhandled Rejection Happened...! Server Is Shutting Down !")
+    if (server) {
+        server.close(() => {
+            process.exit(1)
+        })
+    }
+
+    process.exit(1)
+
+})
 
 process.on("uncaughtException", (err) => {
-  console.log("Uncaught Exception Happened..! Server Is Shutting Down..!", err);
-  if (server) {
-    server.close(() => process.exit(1));
-  } else {
-    process.exit(1);
-  }
-});
+    console.log("Uncaught Exception Happened...! Server Is Shutting Down !", err)
+    if (server) {
+        server.close(() => {
+            process.exit(1)
+        })
+    }
+
+    process.exit(1)
+
+})
