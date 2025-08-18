636e3bf2f34cf2d7aefa0adaf3af76c9c80601c6
jobayer-hossen <emon45088@gmail.com>
2025-08-18 22:18:29 +0600

    vercel update

--- src/server.ts
+++ src/server.ts
@@ -1,77 +1,77 @@
 /* eslint-disable no-console */
-import { Server } from "http"
-
-import mongoose from "mongoose"
+import { Server } from "http";
+import mongoose from "mongoose";
 import app from "./app";
 import { envVars } from "./app/config/env";
+import { connectRedis } from "./app/config/redis.config";
 import { seedSuperAdmin } from "./app/utils/seedSuperAdmin";
 
+let server: Server;
 
-
-let server: Server
-
-
+// Start the server and connect to the database
 const startServer = async () => {
-    try {
-        await mongoose.connect(envVars.DB_URL);
-        console.log("Connected To MongoDb")
-        server = app.listen(envVars.PORT, () => {
-            console.log(`Server is Running On Port ${envVars.PORT}`)
-        })
-    } catch (error) {
-        console.log(error)
-    }
-}
+  try {
+    await mongoose.connect(envVars.DB_URL);
+    console.log("Connected To MongoDb Successfully");
+    server = app.listen(envVars.PORT, () => {
+      console.log(`Server is Running On Port ${envVars.PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
+// Immediately invoke the startServer function to start the server
 (async () => {
-    await startServer()
-    await seedSuperAdmin()
-})()
+  await connectRedis();
+  await startServer();
+  await seedSuperAdmin();
+})();
 
-process.on("SIGTERM", (err) => {
-    console.log("Signal Termination Happened...! Server Is Shutting Down !", err)
-    if (server) {
-        server.close(() => {
-            process.exit(1)
-        })
-    }
+// Handle graceful shutdown
+process.on("SIGTERM", () => {
+  console.log("Signal Termination Happened..! Server Is Shutting Down..!");
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
 
-    process.exit(1)
+  process.exit(1);
+});
 
-})
-
+// Handle manual shutdown and unhandled rejections
 process.on("SIGINT", () => {
-    console.log("I am manually Closing the server! Server Is Shutting Down !")
-    if (server) {
-        server.close(() => {
-            process.exit(1)
-        })
-    }
+  console.log("Signal Interrupt Happened..! Server Is Shutting Down..!");
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
 
-    process.exit(1)
+  process.exit(1);
+});
 
-})
+// Handle unhandled rejections and uncaught exceptions
 process.on("unhandledRejection", () => {
+  console.log("Unhandled Rejection Happened..! Server Is Shutting Down..!");
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
 
-    console.log("Unhandled Rejection Happened...! Server Is Shutting Down !")
-    if (server) {
-        server.close(() => {
-            process.exit(1)
-        })
-    }
+  process.exit(1);
+});
 
-    process.exit(1)
-
-})
-
+// Handle uncaught exceptions
 process.on("uncaughtException", (err) => {
-    console.log("Uncaught Exception Happened...! Server Is Shutting Down !", err)
-    if (server) {
-        server.close(() => {
-            process.exit(1)
-        })
-    }
+  console.log("Uncaught Exception Happened..! Server Is Shutting Down..!", err);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
 
-    process.exit(1)
-
-})
+  process.exit(1);
+});
